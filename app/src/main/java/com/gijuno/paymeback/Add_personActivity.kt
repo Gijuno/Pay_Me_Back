0d9ec67f2d7624ea4345dfdc07b7881a33e258fd
Gijuno <itx.ljh.developer@gmail.com>
2020-06-17 20:41:08 +0900

    Edit Person Activity, Add Person Activity (Base), Main Activity, Intent Activity

---
+++ app/src/main/java/com/gijuno/paymeback/Add_personActivity.kt
@@ -0,0 +1,11 @@
+package com.gijuno.paymeback
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_personActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_person)
+    }
+}
--- app/src/main/java/com/gijuno/paymeback/MainActivity.kt
+++ app/src/main/java/com/gijuno/paymeback/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.gijuno.paymeback
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Goto_Addperson.setOnClickListener {
+            val firstIntent = Intent(this, Add_personActivity::class.java)
+            startActivity(firstIntent)
+        }
+
+        Goto_Editperson.setOnClickListener {
+            val secondIntent = Intent(this, Edit_personActivity::class.java)
+            startActivity(secondIntent)
+        }
     }
 }
